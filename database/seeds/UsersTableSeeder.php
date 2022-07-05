457743b15df7cd6ee1fa6fcfd0f7ba7930328ab9
高橋叶実 <>
2022-07-05 21:14:21 +0900

    シーダー完了

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            [
+                'over_name'    => '高橋',
+                'under_name'   => '叶実',
+                'over_name_kana'   => 'タカハシ',
+                'under_name_kana'   => 'カナミ',
+                'mail_address'   => 'knm@gmail.com',
+                'sex'   =>'2',
+                'role'   =>'4',
+                'birth_day'   =>'19971125',
+                'password'   => bcrypt('password'),
+            ]
 
+          ]);
     }
 }
