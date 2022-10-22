e363dc7bdafbc1ac3fe58377e6fd3817cbc98c66
高橋叶実 <>
2022-10-22 13:39:08 +0900

    リダイレクト先修正

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,9 +14,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
         // 改修課題：タイムアウト時の遷移先をログイン画面へ（下記に書き換える）
         if (! $request->expectsJson()) {
             return route('loginView');
